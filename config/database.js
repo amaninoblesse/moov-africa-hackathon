7e6c76962f484963335b0c48545877d3a0341617
Kouakou Noé AMANI <kouakounoeamani1@gmail.com>
2022-06-16 09:35:04 +0200

    second one

--- config/database.js
+++ config/database.js
@@ -12,19 +12,39 @@
 
 
  // strapi-api/config/database.js
- module.exports = ({ env }) => ({
+//  module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST', 'localhost'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME', 'moov'),
+//       user: env('DATABASE_USERNAME', 'postgres'),
+//       password: env('DATABASE_PASSWORD', '123456'),
+//       schema: env('DATABASE_SCHEMA', 'public'), // Not required
+//       // ssl: {
+//       //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+//       // },
+//     },
+//     debug: false,
+//   },
+// });
+
+
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'moov'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', '123456'),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
-      // ssl: {
-      //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-      // },
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
     },
     debug: false,
   },
